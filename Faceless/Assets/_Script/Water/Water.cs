99de572e9b545de62b5891317b9405b82a521232
Coumn <xavpouiet@hotmail.com>
2015-06-20 21:40:04 -0400

    dunno anymore

--- Faceless/Assets/_Script/Water/Water.cs
+++ Faceless/Assets/_Script/Water/Water.cs
@@ -8,6 +8,7 @@ public class Water : MonoBehaviour{
 	private List<WaterBlock> myBlocks = new List<WaterBlock>();
 	private Transform[] myBlocksTemp;
 	private float idleTime = 10f;
+	private bool idle;
 
 
 	void Start(){
@@ -23,18 +24,19 @@ public class Water : MonoBehaviour{
 	}
 	void Update(){
 		idleTime += 1f*Time.deltaTime;
-		if(idleTime>10f){
-			if(idleTime>15f){
-				idleTime = 10f;
-			}
-			IdleWave(new Vector3(Random.Range(myBlocks[0].gameObject.transform.position.x,myBlocks[myBlocks.Count-1].gameObject.transform.position.x),this.transform.position.y,0f),0.1f);
+		if(idleTime>2f){
+			idle = true;
+			//if(idleTime>2f){
+				idleTime = 1.9f;
+			//}
+			//IdleWave(new Vector3(Random.Range(myBlocks[0].gameObject.transform.position.x,myBlocks[myBlocks.Count-1].gameObject.transform.position.x),this.transform.position.y,0f),0.1f);
 			//IdleWave(new Vector3(Mathf.Lerp(myBlocks[0].gameObject.transform.position.x,myBlocks[myBlocks.Count-1].gameObject.transform.position.x,(idleTime-10f)/5f),this.transform.position.y,0f),0.1f);
 		}
 		foreach(WaterBlock wtb in myBlocks){
 			Vector3[] tempVert;
 			tempVert = wtb.mesh.vertices;
-			tempVert[1] = new Vector3(tempVert[1].x,Mathf.Lerp(tempVert[1].y,wtb.rightV+0.5f,6f*Time.deltaTime) ,tempVert[1].z);
-			tempVert[3] = new Vector3(tempVert[3].x,Mathf.Lerp(tempVert[3].y,wtb.leftV+0.5f,6f*Time.deltaTime),tempVert[3].z);
+			tempVert[1] = new Vector3(tempVert[1].x,Mathf.Lerp(tempVert[1].y,wtb.rightV+0.5f,(idle)?10f*Time.deltaTime:6f*Time.deltaTime) ,tempVert[1].z);
+			tempVert[3] = new Vector3(tempVert[3].x,Mathf.Lerp(tempVert[3].y,wtb.leftV+0.5f,(idle)?10f*Time.deltaTime:6f*Time.deltaTime),tempVert[3].z);
 			if(Approx(tempVert[3].y,wtb.leftV+0.5f,0.1f)){
 				wtb.leftV = wtb.leftV*-0.6f;
 			}
@@ -66,13 +68,14 @@ public class Water : MonoBehaviour{
 
 	void IdleWave(Vector3 pos,float velocity){
 		foreach(WaterBlock wtb in myBlocks){
-			wtb.leftV = Mathf.Clamp((velocity/ Vector3.Distance(wtb.gameObject.transform.position+Vector3.left*0.25f,pos)-1f)*0.1f,-2f,2f);
-			wtb.rightV = Mathf.Clamp((velocity / Vector3.Distance(wtb.gameObject.transform.position+Vector3.right*0.25f,pos)-1f)*0.1f,-2f,2f);
+			wtb.leftV += Mathf.Clamp((velocity/ Vector3.Distance(wtb.gameObject.transform.position+Vector3.left*0.25f,pos)-1f)*0.1f,-1f,1f);
+			wtb.rightV += Mathf.Clamp((velocity / Vector3.Distance(wtb.gameObject.transform.position+Vector3.right*0.25f,pos)-1f)*0.1f,-1f,1f);
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		idleTime = 0f;
+		idle = false;
 		CreateWave(other.transform.position, other.attachedRigidbody.velocity.y);
 
 	}
