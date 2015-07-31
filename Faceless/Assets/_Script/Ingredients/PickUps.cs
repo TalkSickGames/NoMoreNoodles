61866fb1094f1f2f27c316d6a32f9eba42bbfc7a
Coumn <xavpouiet@hotmail.com>
2015-07-30 23:59:28 -0400

    Change in pickups respawn

--- Faceless/Assets/_Script/Ingredients/PickUps.cs
+++ Faceless/Assets/_Script/Ingredients/PickUps.cs
@@ -3,24 +3,25 @@ using System.Collections;
 using Foo;
 
 public class PickUps : MonoBehaviour {
-
+	
 	public PickupType myType;
 	public bool isRespawnable;
 	public float respawnTime;
+	private Vector3 respawnTo;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Gizmos.DrawCube(respawnTo,new Vector3(this.gameObject.GetComponent<BoxCollider2D>().size.x,this.gameObject.GetComponent<BoxCollider2D>().size.x,this.gameObject.GetComponent<BoxCollider2D>().size.x));
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.gameObject.tag == "Player"){
 			switch(myType){
-
+				
 			case PickupType.Ammo :
 				GameManager.Instance.AvatarB.PowerAmmo += 1;
 				break;
@@ -31,11 +32,32 @@ public class PickUps : MonoBehaviour {
 				GameManager.Instance.AvatarB.HP += 2;
 				break;
 			}
+			
 			if(isRespawnable){
-				GameManager.Instance.gameObject.GetComponent<RespawnManager>().MyRespawn.Add( new Respawnable(myType, this.gameObject.transform.position, respawnTime));
+				respawnTo = this.transform.position;
+				Invoke("Respawn",respawnTime);
+				this.transform.position = Vector3.up*99999f;
+			}else{
+				Destroy(this.gameObject);
 			}
-			Destroy(this.gameObject);
+			
 		}
-
+		
+	}
+	
+	//	void OnDrawGizmos(){
+	//		Gizmos.DrawCube(respawnTo,new Vector3(this.gameObject.GetComponent<BoxCollider2D>().size.x,this.gameObject.GetComponent<BoxCollider2D>().size.x,this.gameObject.GetComponent<BoxCollider2D>().size.x));
+	//
+	//	}
+	
+	void Respawn(){
+		//if(!Physics2D.BoxCast(new Vector2(respawnTo.x,respawnTo.y),new Vector2(0.5f,0.5f),0f,Vector2.up)){
+		this.transform.position = respawnTo;
+		//		}else{
+		//		
+		//			Debug.Log("sad");
+		//			Invoke("Respawn",1f);
+		//		}
+		
 	}
 }
