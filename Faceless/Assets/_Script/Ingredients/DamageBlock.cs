c9c7bcbc2d0c86e41f8860a199b2674817522720
Coumn <xavpouiet@hotmail.com>
2015-09-09 20:12:20 -0400

    tweak + effect dmg and stuff + spiky-spike! D:

---
+++ Faceless/Assets/_Script/Ingredients/DamageBlock.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageBlock : MonoBehaviour {
+	public int damage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.GetComponent<Avatar_BehaviorV2>() != null){
+			coll.gameObject.GetComponent<Avatar_BehaviorV2>().TakeDamage(damage,(float)damage*10f,this.transform.position);
+		}
+		
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.GetComponent<Avatar_BehaviorV2>() != null){
+			coll.gameObject.GetComponent<Avatar_BehaviorV2>().TakeDamage(damage,(float)damage*10f,this.transform.position);
+		}
+		
+	}
+}
