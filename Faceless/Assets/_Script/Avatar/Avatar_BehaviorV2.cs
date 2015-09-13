b543586182b0d3f24fbaa90f7877143a457a3536
Coumn <xavpouiet@hotmail.com>
2015-09-12 23:32:00 -0400

    forSame

--- Faceless/Assets/_Script/Avatar/Avatar_BehaviorV2.cs
+++ Faceless/Assets/_Script/Avatar/Avatar_BehaviorV2.cs
@@ -228,8 +228,8 @@ public class Avatar_BehaviorV2 : MonoBehaviour {
 		isLedge = false;
 		if(movement.y <=2f && !Physics2D.Raycast(this.transform.position,Vector2.down,1.2f,groundLayer) && !isInShot){
 			//isLedge = false;
-			if(Physics2D.Raycast(this.transform.position+(Vector3.left*0.25f)+(Vector3.up*1f),Vector2.down,1f,groundLayer)){
-				Vector2 tempPoint = Physics2D.Raycast(this.transform.position+(Vector3.left*0.25f)+(Vector3.up*1f),Vector2.down,1f,groundLayer).point;
+			if(Physics2D.Raycast(this.transform.position+(Vector3.left*0.25f)+(Vector3.up*1.5f),Vector2.down,1.5f,groundLayer)){
+				Vector2 tempPoint = Physics2D.Raycast(this.transform.position+(Vector3.left*0.25f)+(Vector3.up*1.5f),Vector2.down,1.5f,groundLayer).point;
 				if(!Physics2D.Raycast(tempPoint+(Vector2.up*0.1f),Vector2.left,0.5f,groundLayer) && Input.GetAxisRaw ("Horizontal")<-0.2f){
 					this.transform.position = tempPoint-(Vector2.up*0.8f)+(Vector2.right*0.25f);
 //			if(Physics2D.Raycast(this.transform.position+(Vector3.left*0.25f)+(Vector3.up*1f),Vector2.down,0.5f,groundLayer)){
@@ -246,8 +246,8 @@ public class Avatar_BehaviorV2 : MonoBehaviour {
 				
 			}
 			
-			if(Physics2D.Raycast(this.transform.position+(Vector3.right*0.25f)+(Vector3.up*1f),Vector2.down,1f,groundLayer)){
-				Vector2 tempPoint = Physics2D.Raycast(this.transform.position+(Vector3.right*0.25f)+(Vector3.up*1f),Vector2.down,1f,groundLayer).point;
+			if(Physics2D.Raycast(this.transform.position+(Vector3.right*0.25f)+(Vector3.up*1.5f),Vector2.down,1.5f,groundLayer)){
+				Vector2 tempPoint = Physics2D.Raycast(this.transform.position+(Vector3.right*0.25f)+(Vector3.up*1.5f),Vector2.down,1.5f,groundLayer).point;
 				if(!Physics2D.Raycast(tempPoint+(Vector2.up*0.1f),Vector2.right,0.5f,groundLayer) && Input.GetAxisRaw ("Horizontal")>0.2f){
 					this.transform.position = tempPoint-(Vector2.up*0.8f)+(Vector2.left*0.25f);
 					isLedge = true;
