1bbcd5e092e208a6df45d77bb7194f356c6f15f2
Coumn <xavpouiet@hotmail.com>
2015-09-16 20:43:16 -0400

    tweak on Avatar, more effect

--- Faceless/Assets/_Script/UI/Canvas_UI.cs
+++ Faceless/Assets/_Script/UI/Canvas_UI.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 
 public class Canvas_UI : MonoBehaviour {
 	public GameObject[] hearts;
-	public GameObject[] trickAmmo;
-	public GameObject[] powerAmmo;
-	public GameObject[] timeAmmo;
-
-	public GameObject trickCursor;
-	public GameObject powerCursor;
-	public GameObject[] masks;
-	public GameObject[] sMasks;
+	public GameObject[] ammos;
+//	public GameObject[] trickAmmo;
+//	public GameObject[] powerAmmo;
+//	public GameObject[] timeAmmo;
+//
+//	public GameObject trickCursor;
+//	public GameObject powerCursor;
+//	public GameObject[] masks;
+//	public GameObject[] sMasks;
 	// Use this for initialization
 	void Start () {
 
@@ -21,18 +22,18 @@ public class Canvas_UI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetAxis("DpadH") < -0.2f || Input.GetAxis("DpadH") > 0.2f || Input.GetAxis("DpadV") < -0.2f || Input.GetAxis("DpadV") > 0.2f){
-		//if(Input.GetButton("DpadU") || Input.GetButton("DpadR") || Input.GetButton("DpadD") || Input.GetButton("DpadL")){
-			masks[0].SetActive(true);
-			masks[1].SetActive(true);
-			masks[2].SetActive(true);
-			masks[3].SetActive(true);
-		}else{
-			masks[0].SetActive(false);
-			masks[1].SetActive(false);
-			masks[2].SetActive(false);
-			masks[3].SetActive(false);
-		}
+//		if(Input.GetAxis("DpadH") < -0.2f || Input.GetAxis("DpadH") > 0.2f || Input.GetAxis("DpadV") < -0.2f || Input.GetAxis("DpadV") > 0.2f){
+//		//if(Input.GetButton("DpadU") || Input.GetButton("DpadR") || Input.GetButton("DpadD") || Input.GetButton("DpadL")){
+//			masks[0].SetActive(true);
+//			masks[1].SetActive(true);
+//			masks[2].SetActive(true);
+//			masks[3].SetActive(true);
+//		}else{
+//			masks[0].SetActive(false);
+//			masks[1].SetActive(false);
+//			masks[2].SetActive(false);
+//			masks[3].SetActive(false);
+//		}
 
 //		sMasks[0].SetActive(((int)GameManager.Instance.AvatarB.MyMask == 0)?true:false);
 //		sMasks[1].SetActive(((int)GameManager.Instance.AvatarB.MyMask == 1)?true:false);
@@ -40,10 +41,10 @@ public class Canvas_UI : MonoBehaviour {
 //		sMasks[3].SetActive(((int)GameManager.Instance.AvatarB.MyMask == 3)?true:false);
 //
 //		trickCursor.SetActive(GameManager.Instance.AvatarB.ChargeIsTrick);
-		trickAmmo[0].SetActive((GameManager.Instance.AvatarB.Ammo >= 1)?true:false);
-		trickAmmo[1].SetActive((GameManager.Instance.AvatarB.Ammo >= 2)?true:false);
-		trickAmmo[2].SetActive((GameManager.Instance.AvatarB.Ammo >= 3)?true:false);
-		trickAmmo[3].SetActive((GameManager.Instance.AvatarB.Ammo >= 4)?true:false);
+		ammos[0].SetActive((GameManager.Instance.AvatarB.Ammo >= 1)?true:false);
+		ammos[1].SetActive((GameManager.Instance.AvatarB.Ammo >= 2)?true:false);
+		ammos[2].SetActive((GameManager.Instance.AvatarB.Ammo >= 3)?true:false);
+		ammos[3].SetActive((GameManager.Instance.AvatarB.Ammo >= 4)?true:false);
 //		powerAmmo[0].SetActive((GameManager.Instance.AvatarB.PowerAmmo >= 1)?true:false);
 //		powerAmmo[1].SetActive((GameManager.Instance.AvatarB.PowerAmmo >= 2)?true:false);
 //		powerCursor.SetActive(!GameManager.Instance.AvatarB.ChargeIsTrick);
---
+++ Faceless/Assets/testDash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testDash : MonoBehaviour {
+	private bool goingLeft;
+	//private Vector2 goingTo;
+	private float tTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tTime += Time.deltaTime;
+		if(tTime >= 1f){
+			tTime = 0f;
+			goingLeft = !goingLeft;
+		}
+		if(goingLeft){
+			this.transform.Translate(-2f*Time.deltaTime,0f,0f);
+		}else{
+			this.transform.Translate(2f*Time.deltaTime,0f,0f);
+		}
+	}
+}
